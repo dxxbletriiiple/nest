3faf9caff9a03a50c40ff8c1230a2af66c63b693
dxxbletriiiple <dxxbletriiiple@gmail.com>
2024-08-07 18:31:27 +0300

    add: typeorm and pg db

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesModule } from 'src/coffees/coffees.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-	imports: [CoffeesModule],
+	imports: [
+		CoffeesModule,
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			username: 'postgres',
+			password: 'PASS123',
+			database: 'postgres',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
